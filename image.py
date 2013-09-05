53f887c32496628a1173daf3d4595c9ac4ca6a9c
Haelwenn Monnier <lanodan.delta@free.fr>
2013-09-05 08:14:45 +0200

    Create image.py

---
+++ image.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import gtk
+
+class Image:
+    def __init__(self):
+        window = gtk.Window()
+        window.set_default_size(640, 480)
+        
+        vbox = gtk.VBox(False, 5)
+        
+        scrolledwindow = gtk.ScrolledWindow()
+        
+        self.image = gtk.Image()       
+        self.button = gtk.FileChooserButton("Select Image", None)
+        
+        self.button.connect("file-set", self.load_image)
+        
+        window.add(vbox)
+        vbox.pack_start(self.button, False, False, 0)
+        vbox.pack_start(scrolledwindow, True, True, 0)
+        
+        scrolledwindow.add_with_viewport(self.image)
+        
+        window.show_all()
+    
+    def load_image(self, widget):
+        self.image.set_from_file(self.button.get_filename())
+
+Image()
+gtk.main()
